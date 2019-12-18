7cb45b805040db3301ed229550de6dc71cc81ebe
Vecheslav Druzhbin <vecheslavdruzhbin@gmail.com>
2019-12-19 02:51:37 +0300

    init docs

---
+++ services/docs-web/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Item Protocol Documentation',
+  description: 'How to use Item Protocol',
+  themeConfig: {
+    docsRepo: 'wavesplatform/item',
+    docsDir: 'docs',
+  },
+}
